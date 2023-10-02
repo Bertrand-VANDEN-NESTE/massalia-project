d92c2f8a99e85582900b7638977029b6d9b3ee0a
Bertrand VANDEN NESTE <bertrand.vanden.neste@gmail.com>
2023-10-02 13:04:26 +0200

    modification of css and add divs in html

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Entry point for the build script in your package.json
 //= require carousel
 //= require btnburger
+//= require filtercategory
---
+++ app/javascript/filtercategory.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const categoryButtons = document.querySelectorAll('.category-button');
+  const categoryDropdown = document.getElementById('categoryDropdown');
+  const categoryInput = document.querySelector('.form-search-category input[name="category"]');
+  const categoryForm = document.querySelector('.form-search-category');
+
+  categoryDropdown.addEventListener('click', function () {
+    categoryDropdown.classList.toggle('active');
+  });
+
+  categoryButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      // Obtenir la catégorie sélectionnée et la soumettre au formulaire
+      const selectedCategory = button.getAttribute('data-category');
+      categoryInput.value = selectedCategory;
+      // Soumettre automatiquement le formulaire pour effectuer le filtrage
+      categoryForm.submit();
+    });
+  });
+});
